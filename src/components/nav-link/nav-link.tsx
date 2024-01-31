ade9225d36217a2fcd73929f48976a2c608df1a8
Kevin Barreto <barretokevin@hotmail.com>
2024-01-31 17:05:28 -0600

    fix prop

--- src/components/nav-link/nav-link.tsx
+++ src/components/nav-link/nav-link.tsx
@@ -3,16 +3,26 @@ import Link from "next/link";
 import styles from "./nav-link.module.css";
 import { Josefin_Sans } from "next/font/google";
 
+interface Props {
+  scrolled: boolean;
+}
+
 const josefin = Josefin_Sans({
   subsets: ["latin"],
   weight: ["500"],
 });
 
-export function NavLink() {
+export function NavLink({ scrolled }: Props) {
   return (
     <div className={josefin.className}>
       {navbarLinks.map((link) => (
-        <Link className={styles["nav-link"]} href={link.link} key={link.id}>
+        <Link
+          className={`${styles["nav-link"]} ${
+            scrolled ? styles["scrolled"] : ""
+          }`}
+          href={link.link}
+          key={link.id}
+        >
           {link.children}
         </Link>
       ))}
